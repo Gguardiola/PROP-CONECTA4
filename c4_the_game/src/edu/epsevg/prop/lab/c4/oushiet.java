eef43eb90aa413479827e2d5d693145975bb59a6
Hospuntake <91428070+Hospuntake@users.noreply.github.com>
2022-11-16 13:50:52 +0100

    definitivo 100% acabadisimo no tocar q se rompe

--- c4_the_game/src/edu/epsevg/prop/lab/c4/oushiet.java
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/oushiet.java
@@ -11,20 +11,10 @@ public class oushiet
   private String nom;
   private int torn=1;
   private int cont=0;
-  public int[][] tablaPuntuacio = {
-        {3, 4, 5, 7, 7, 5, 4, 3},
-        {4, 6, 8,10,10, 8, 6, 4},
-        {5, 8,11,13,13,11, 8, 5},
-        {7,10,13,16,16,13,10, 7},
-        {7,10,13,16,16,13,10, 7},
-        {5, 8,11,13,13,11, 8, 5},
-        {4, 6, 8,10,10, 8, 6, 4},
-        {3, 4, 5, 7, 7, 5, 4, 3}
-    };
-
-
-  public oushiet()
+  int profmax=5;
+    public oushiet(int profmax1)
   {
+    profmax=profmax1;
     nom = "oushiet";
   }
   
@@ -32,9 +22,8 @@ public class oushiet
   public int moviment(Tauler t, int color)
   {
     torn=color;
-    int col=minimax(t,5);
+    int col=minimax(t,profmax);
       System.out.println("cont: "+cont);
-      System.out.println("col: "+col);
     return col;
     
   }
@@ -46,13 +35,10 @@ public class oushiet
   }
    public int minimax(Tauler taulell, int profunditat){
         int val=Integer.MIN_VALUE;  
-        int columna=0;//SE PUEDE CAMBIAR EL VALORA PARA ESRUDIAR EL COPORTAMIENTO 
+        int columna=4;
         //Definim la funcio inici de la crida miniamx, es a dir, la funció que iniciará
         //la crida.
-        //Primerament definim els valors alfa i beta y el numero de columna(useless aquest ultim)
-        //val es el valor con el que se incia esta poda , es decir , el que nos subiamos y bajabamos
-        //siempre en los ejercicios de minmax
-        for (int i = 0; i < taulell.getMida(); ++i){
+            for (int i = 0; i < taulell.getMida(); ++i){
             //recorremos todo el taulell creando nuevas jugadas
             if(taulell.movpossible(i)){
                 //si el moviemiento es posible , entonces creamos esta nueva jugada
@@ -60,13 +46,13 @@ public class oushiet
                Tauler noutaulell=new Tauler(taulell);
                noutaulell.afegeix(i,torn);
                //obviamente tendremos que ponerle el siguiente estado
-               int valSeg = cridaMin(taulell,Integer.MIN_VALUE,Integer.MAX_VALUE,i,profunditat-1);
+               int valSeg = cridaMin(noutaulell,Integer.MIN_VALUE,Integer.MAX_VALUE,i,profunditat-1);
                //esta llamada es donde pediremos el valor obtenido con la llamada la los hijos siguientes
                if(valSeg > val){
-                   //comprobamos la poda?¿?¿?¿?¿
-                   //RELEEEEEEEEEEEEEEEER!!!!!!!!!!!!!!!
+                   //coge el maximo de todos
                     val = valSeg;
                     columna = i;
+                    //y pillamos q esa columna es la adecuada
                 }     
             }     
         }
@@ -88,15 +74,14 @@ public class oushiet
            return Integer.MIN_VALUE+83647;
         }
         //si ja s'ha acabat la profunditat o el taulell esta ple , llavors cridem a la heuristica
-        else if ((profunditat <= 0 )|| (!(taulell.espotmoure()))){
+        else if ((profunditat == 0 ) || (!(taulell.espotmoure()))){
             return Heuristica(taulell);
         }
         //Definim els valors val i preparem un bool per poder comprabar despres si podem o no 
         int val=Integer.MAX_VALUE;
-        Boolean PodacionDelAmazonas=false;
         for(int i = 0; i < taulell.getMida(); i++){
             //mentres poguem fer el moviment i no hagi poda...
-            if(taulell.movpossible(i) && !(PodacionDelAmazonas)){
+            if(taulell.movpossible(i)){
                 /*Creem un nou taulell amb el seguent posible moviment (ara de l'enemic)
                 per tal de poder fer la proxima crida. Ara toca fer la crida a
                 l'algoritme max, ja que despres del min va el max jaja
@@ -108,11 +93,11 @@ public class oushiet
                 al mateix moment, agafem el numero mes petit i l'actualitzem a la variable
                 per mirar si fem la poda alfabeta
                 */
-                int auxVal=cridaMax(taulell,alfa,beta, i,profunditat-1);
+                int auxVal=cridaMax(noutaulell,alfa,beta, i,profunditat-1);
                 val=Math.min(val,auxVal);
                 beta=Math.min(val,beta);
-                //si alfa es major a beta llavors podem
-                if(alfa>/*=¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿*/beta)PodacionDelAmazonas=true;
+                //si alfa es major a beta llavors podem i ens petem tot lo altre
+                if(alfa>=beta)break;
              }
         }
         return val;
@@ -131,62 +116,143 @@ public class oushiet
            return Integer.MIN_VALUE+83647;
         }
         //si ja s'ha acabat la profunditat o el taulell esta ple , llavors cridem a la heuristica
-        else if ((profunditat <= 0 )|| (!(taulell.espotmoure()))){
+        else if ((profunditat == 0 )|| (!(taulell.espotmoure()))){
             return Heuristica(taulell);
         }
         
         //Definim els valors val i preparem un bool per poder comprabar despres si podem o no 
         int val=Integer.MIN_VALUE;
-        Boolean PodacionDelAmazonas=false;
         for(int i = 0; i < taulell.getMida(); i++){
             //mentres poguem fer el moviment i no hagi poda...
-            if(taulell.movpossible(i) && !(PodacionDelAmazonas)){
+            if(taulell.movpossible(i) ){
                 /*Creem un nou taulell amb el seguent posible moviment (ara de l'enemic)
                 per tal de poder fer la proxima crida. Ara toca fer la crida a
-                l'algoritme max, ja que despres del min va el max jaja
+                l'algoritme min, ja que despres del max va el min jaja
                 */
                 Tauler noutaulell = new Tauler(taulell);
                 noutaulell.afegeix(i, torn);
                 /*
-                Cridem la crida max per tal de trobar un altre valor de l'heuristica 
-                al mateix moment, agafem el numero mes petit i l'actualitzem a la variable
+                Cridem la crida min per tal de trobar un altre valor de l'heuristica 
+                al mateix moment, agafem el numero mes gran i l'actualitzem a la variable
                 per mirar si fem la poda alfabeta
                 */
-                int auxVal=cridaMin(taulell,alfa,beta, i,profunditat-1);
+                int auxVal=cridaMin(noutaulell,alfa,beta, i,profunditat-1);
                 val=Math.max(val,auxVal);
-                beta=Math.max(val,beta);
-                //si alfa es major a beta llavors podem
-                if(alfa>/*=¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿*/beta)PodacionDelAmazonas=true;
+                alfa=Math.max(val,alfa);
+                //si alfa es major a beta llavors podem i ens petem el bucle
+                if(alfa>=beta)break;
              }
         }
         return val;
         }
 
-  public int Heuristica(Tauler pt){
-      ++cont;
-      int valorHeur = 0;
-        for (int i = 0; i < 8; i++) {
-            if(pt.getColor(i, 0)==0 && pt.getColor(i,1)==0  &&
-                pt.getColor(i, 2)==0 && pt.getColor(i,3)==0 &&
-                 pt.getColor(i, 4)==0 && pt.getColor(i,5)==0 &&
-                     pt.getColor(i, 6)==0 && pt.getColor(i,7)==0){
-                break;
-            }
-            for (int j = 0; j < 8; j++) {
-                if (pt.getColor(i, j) == torn) {
-                    valorHeur += tablaPuntuacio[i][j];
-                } else if (pt.getColor(i, j) == torn*-1) {
-                    valorHeur -= tablaPuntuacio[i][j];
-                }
-            }
-        }
-        return valorHeur;
+    public int Heuristica(Tauler t){
+        ++cont;
+        //Sumem totes les heuristiques posibles de les posibles combinacions 
+        //que es poden fer.
+        int heuristica=HeurChorizontales(t)+ HeurBertuscales(t) + HeurDeIzqaDerDiagonal(t) + HeurDeDeraIzqDiagonal(t);
+        return heuristica;
 
   
   }
+    
+    public int HeurChorizontales(Tauler t){
+        int heur=0;
+        for (int i = 0; i < t.getMida()-3; i++) {
+            for (int j = 0; j < t.getMida(); j++) {
+                int NumFitxesUtils=0;
+                for(int k=0;k<4;++k){
+                    //aquest for mira les 4 files seguides, si està 
+                    if(t.getColor(i+k, j) == torn ){
+                        ++NumFitxesUtils;
+                       //si en una mateixa fila,hi han x fitxes meves, anem sumant
+                       //per l'heur.
+                    }
+                    if(t.getColor(i+k, j) == -torn){
+                        --NumFitxesUtils;
+                        //si tenim una fitxa de l'enemic, llavors anem restant lentament.   
+                    }
+                }
+                heur=heur+NumFitxesUtils;
+            } 
+        }  
+      return heur;
+  }
  
+    public int HeurBertuscales(Tauler t){
+        //lo mismo pa vertiacles l
+        int heur=0;
+        for (int i = 0; i < t.getMida(); i++) {
+            for (int j = 0; j < t.getMida()-3; j++) {
+                int NumFitxesUtils=0;
+                for(int k=0;k<4;++k){
+                    if(t.getColor(i, j+k) == torn ){
+                        ++NumFitxesUtils;
+                      
+                    }
+                    if(t.getColor(i, j+k) == -torn){
+                        --NumFitxesUtils;
+                          
+                    }
+                }
+                heur=heur+NumFitxesUtils;
+            } 
+        }  
+      return heur;
+        }
+    
+    public int HeurDeIzqaDerDiagonal(Tauler t){
+        //ahora para lineas de . a l
+        int heur=0;
+        for (int i = 0; i < t.getMida()-3; i++) {
+            
+            for (int j = 0; j < t.getMida()-3; j++) {
+                int NumFitxesUtils=0;
+                for(int k=0;k<4;++k){
+                    if(t.getColor(i+k, j+k) == torn ){
+                        ++NumFitxesUtils;
+                    }
+                    if(t.getColor(i+k, j+k) == -torn){
+                        --NumFitxesUtils;
+                   
+                    }
+                }
+                heur=heur+NumFitxesUtils;
+            } 
+        }  
+      return heur;
+        }
+    public int HeurDeDeraIzqDiagonal(Tauler t){
+        //ahora de l a .
+        int heur=0;
+
+        for (int i = 3; i < t.getMida(); i++) {
+                                    
+
+            for (int j = 0; j < t.getMida()-3; j++) {
+                int NumFitxesUtils=0;
+                for(int k=0;k<4;++k){
+                    //aquest for mira les 4 files seguides, si està 
+                    if(t.getColor(i-k, j+k) == torn ){
+                        ++NumFitxesUtils;
+                       //si en una mateixa fila,hi han x fitxes meves, anem sumant
+                       //per l'heur.
+                    }
+                    if(t.getColor(i-k, j+k) == -torn){
+                        --NumFitxesUtils;
+                        //si tenim una fitxa de l'enemic, llavors els parells queden
+                        //invalids i ens petem lo demes d'aquest bucle concret.   
+                    }
+                }
+                heur=heur+NumFitxesUtils;
+            } 
+        }  
+      return heur;
+    }
+
+    
+    
     
 
 }
-
 
